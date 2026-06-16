592bb1fea9a6c2e4374bd1f3f43e4c79d4b92343
Rina-trabajo99 <rinakoba.trabajo@gmail.com>
2026-06-17 00:56:12 +0900

    upd: 配列の追加・削除の処理の実装

--- react-basic-practice/src/App.tsx
+++ react-basic-practice/src/App.tsx
@@ -7,67 +7,78 @@ type Task = {
 
 function App() {
   const [taskName, setTaskName] = useState("");
-  const [tasks, setTasks] = useState<Task[]>([]);
-
-  const [taskNameError, setTaskNameError] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: 1, name: "React useState復習" },
+    { id: 2, name: "Java List復習" },
+    { id: 3, name: "SQL SELECT練習" },
+  ]);
+  const [errorMsg, setErrorMsg] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
 
   const handleChangeTaskName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTaskName(e.target.value);
-    setTaskNameError("");
-    setSuccessMessage("");
+    const inputValue = e.target.value;
+
+    setTaskName(inputValue);
+
+    //テキストが入力された際にメッセージを空にする
+    if (inputValue.trim() !== "") {
+      setErrorMsg("");
+      setSuccessMsg("");
+    }
   };
 
   const handleAddTask = () => {
+    //削除メッセージは何かしらのアクションがあった際は常に空にする
+    setSuccessMsg("");
+
     if (taskName.trim() === "") {
-      setTaskNameError("タスク名を入力してください。");
-      setSuccessMessage("");
+      setErrorMsg("タスクを入力してください。");
       return;
     }
 
     const newTask: Task = {
       id: Date.now(),
-      name: taskName.trim(),
+      name: taskName,
     };
 
     setTasks([...tasks, newTask]);
+    setSuccessMsg("タスクを追加しました。");
     setTaskName("");
-    setTaskNameError("");
-    setSuccessMessage("タスクを追加しました。");
+    setErrorMsg("");
+  };
+
+  const handleDeleteTask = (deleteId: number) => {
+    const newTasks = tasks.filter((task) => task.id !== deleteId);
+    setTasks(newTasks);
+    setSuccessMsg("タスクを削除しました。");
   };
 
   return (
     <div>
-      <h1>学習タスク管理</h1>
+      <h1>学習タスク一覧</h1>
 
-      <div>
-        <div>
-          <label>
-            タスク名：
-            <input value={taskName} onChange={handleChangeTaskName} />
-          </label>
+      <input
+        value={taskName}
+        onChange={handleChangeTaskName}
+        placeholder="タスク名を入力"
+      />
+      <button onClick={handleAddTask}>追加</button>
+      {errorMsg !== "" && <p>{errorMsg}</p>}
 
-          <button onClick={handleAddTask}>追加</button>
-        </div>
-        {taskNameError !== "" && <p>{taskNameError}</p>}
-      </div>
+      {tasks.length === 0 && <p>タスクはまだ登録されていません。</p>}
+      {tasks.length !== 0 && <p>登録件数：{tasks.length}件</p>}
 
-      <section>
-        <h2>タスク一覧</h2>
-
-        <p>
-          タスク件数：
-          {tasks.length}件
-        </p>
-
+      {tasks.length > 0 && (
         <ul>
           {tasks.map((task) => (
-            <li key={task.id}>{task.name}</li>
+            <li key={task.id}>
+              {task.name}
+              <button onClick={() => handleDeleteTask(task.id)}>削除</button>
+            </li>
           ))}
         </ul>
-      </section>
-
-      {successMessage !== "" && <p>{successMessage}</p>}
+      )}
+      {successMsg !== "" && <p>{successMsg}</p>}
     </div>
   );
 }
